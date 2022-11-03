11a69434616712711e0cd3331dd914702422b96d
rogseo <rogseo@gmail.com>
2022-11-02 22:01:47 -0500

    catergory route: get('/'),get('/:id') added

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -19,16 +19,18 @@ Category.hasMany(Product,{
 Product.belongsToMany(Tag,{
   through:{
     model: ProductTag,
-    unique: false
+    unique: false,
   },
+  // as:'product_tag'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
   through: {
     model: ProductTag,
-    unique : false
+    unique : false,
   },
+  // as:'tag_product'
 
 });
 
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,16 +1,38 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { findAll, findByPk } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try{
+    const categoryData= await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  }catch(err){
+    res.status(500).json(err);
+  }
+
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try{
+    const categoryData=findByPk(req.params.id,{
+      include:[{model:Product}],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  }catch(err){
+    res.status(500).json(err);
+  }
+  
 });
 
 router.post('/', (req, res) => {
